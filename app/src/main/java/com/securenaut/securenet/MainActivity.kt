c35f7ce434de673770bbe64e629f1af7e256539e
Yashi Natu <yashinatu2007@gmail.com>
2023-12-12 10:37:29 +0530

    app list static analysis

--- app/src/main/java/com/securenaut/securenet/MainActivity.kt
+++ app/src/main/java/com/securenaut/securenet/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.securenaut.securenet
-
+import StaticAnalysisAppList
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StaticAnalysisAppList()
                 }
             }
         }
