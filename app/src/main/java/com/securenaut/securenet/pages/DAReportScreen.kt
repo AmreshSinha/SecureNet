cca6012c9635da470a0c6aaeab716af65a4b7b82
Siddhartha kumar singh <siddharthakumarsingh946@gmail.com>
2023-12-19 10:44:30 +0530

    daReport upper Card created

--- app/src/main/java/com/securenaut/securenet/pages/DAReportScreen.kt
+++ app/src/main/java/com/securenaut/securenet/pages/DAReportScreen.kt
@@ -1,9 +1,56 @@
 package com.securenaut.securenet.pages
 
+import AppBar
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.securenaut.securenet.R
+import com.securenaut.securenet.ui.theme.Typography
 
 @Composable
 fun DAReportScreen(navController: NavController) {
-
+    Scaffold(topBar = {
+        AppBar(navController = navController, name = "Dynamic Analysis", onBackScreen = "home")
+    }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding)
+                .padding(horizontal = 16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Column {
+                    Image(
+                        painter = painterResource(id = R.drawable.icon),
+                        contentDescription = "Icon Image"
+                    )
+                    Text(
+                        text = "Instagram",
+                        style = Typography.headlineMedium
+                    )
+                }
+            }
+        }
+    }
 }
