402a9cf4bde918c1161be4fbf16f9cdfcdb3ccf0
Siddhartha kumar singh <siddharthakumarsingh946@gmail.com>
2023-12-20 07:27:36 +0530

    url home set

--- app/src/main/java/com/securenaut/securenet/MainActivity.kt
+++ app/src/main/java/com/securenaut/securenet/MainActivity.kt
@@ -97,6 +97,7 @@ class MainActivity() : ComponentActivity() {
                 this, Manifest.permission.POST_NOTIFICATIONS) -> {
                 // In an educational UI, explain to the user why your app requires this
                 // permission for a specific feature to behave as expected, and what
+                // permission for a specific feature to behave as expected, and what
                 // features are disabled if it's declined. In this UI, include a
                 // "cancel" or "no thanks" button that lets the user continue
                 // using your app without granting the permission.
